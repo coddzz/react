8a0cffdeceba9b4fe55782102dc2c5715262a8a7
Sarath <learn.cypher@gmail.com>
2025-03-25 17:40:23 +0530

    Math.random - App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 function App() {
+
+  function handleNameChange(){
+
+    const names = ['Black','White','Red','Green','Blue'];
+    const int = Math.floor(Math.random()*5);
+    return names[int]
+  }
+
   return (
     <div>
       <i>
-        Hello World !!
+        <b>Hello World !!</b>
+        <p>Refresh to change names</p>
+        <p>Colours: {handleNameChange()}  </p>
       </i>
     </div>
   )
