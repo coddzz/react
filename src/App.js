ffdd75375c626fafd88c61dd648999ce5dde75a0
Sarath <learn.cypher@gmail.com>
2025-03-30 22:59:22 +0530

    import Header File

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
+import Header from "./Header";
+
 function App() {
-
-  function handleNameChange(){
-
-    const names = ['Black','White','Red','Green','Blue'];
-    const int = Math.floor(Math.random()*5);
-    return names[int]
-  }
 
   return (
     <div>
-      <i>
-        <b>Hello World !!</b>
-        <p>Refresh to change names</p>
-        <p>Colours: {handleNameChange()}  </p>
-      </i>
+      <Header />
     </div>
   )
 }
