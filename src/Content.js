5fa07db5ea6f55dbf966e8a0c2c6f8aa809bf342
Sarath <learn.cypher@gmail.com>
2025-04-09 23:22:57 +0530

    useState Hooks

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
 
+    const [coloursName, setName] = useState("Black");
     function handleNameChange() {
         const names = ['Black','White','Red','Green','Blue'];
         const int = Math.floor(Math.random()*5);
-        return names[int];
+        setName(names[int]);
     }
 
     const handleClick = () =>{
@@ -20,10 +21,11 @@ const Content = () => {
             <i>
                 <b>Hello World !!</b>
                 <p>Refresh to change names</p>
-                <p>Colours: {handleNameChange()} </p>
+                <button onClick={handleNameChange}>Colours Name </button>
+                <p>{coloursName}</p>
             </i>
             <button onClick={handleClick}> Button 1 </button>
-            <button onDoubleClick={() => handleClick2("Hello World!!")}> Button 2 </button>
+            <button onDoubleClick={() => handleClick2("Hello World!!")}> Button 2 (Double click)</button>
         </main>
     )
 }
